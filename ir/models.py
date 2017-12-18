71658bae0abe7293f0309140de1abd9940532dfa
Alva Thomson <alvath96@localhost.localdomain>
2017-12-18 20:34:20 +0700

    add similarities algorithms

--- ir/models.py
+++ ir/models.py
@@ -2,7 +2,7 @@ import numpy as np
 from tqdm import tqdm
 
 from utils import make_vector, get_tf_idf, get_tf_for_doc, get_tf_idf_for_doc, get_tf_for_query, sorted_by_value, get_ranking_with_info, dump_to_pickle, load_from_pickle
-from similarities import cosine
+from similarities import sim
 
 class VSM:
     def __init__(self, corpus, tf, idf, tfidf, idf_artist, tfidf_artist, idf_genre, tfidf_genre, idf_title, tfidf_title):
@@ -31,17 +31,17 @@ class VSM:
     def get_doc_ids(self):
         return [ doc['index'] for doc in self.corpus]
 
-    def get_scores_tf(self, query):
+    def get_scores_tf(self, query, sim_algo='cosine'):
         query_vector = make_vector(get_tf_for_query(self.tf, query))
-        scores = { doc_id: cosine(query_vector, make_vector(get_tf_for_doc(self.tf, doc_id))) for doc_id in tqdm(self.get_doc_ids()) }
+        scores = { doc_id: sim(query_vector, make_vector(get_tf_for_doc(self.tf, doc_id)), sim_algo) for doc_id in tqdm(self.get_doc_ids()) }
         return sorted_by_value(scores)
 
-    def get_scores_tf_idf(self, query):
+    def get_scores_tf_idf(self, query, sim_algo='cosine'):
         query_vector = make_vector(self.get_tf_idf(get_tf_for_query(self.tf, query)))
-        scores = { doc_id : cosine(query_vector, self.doc_tf_idf_vectors[doc_id]) for doc_id in tqdm(self.get_doc_ids()) }
+        scores = { doc_id : sim(query_vector, self.doc_tf_idf_vectors[doc_id], sim_algo) for doc_id in tqdm(self.get_doc_ids()) }
         return sorted_by_value(scores)
 
-    def get_scores_tf_idf_weighted(self, query, weight):
+    def get_scores_tf_idf_weighted(self, query, weight, sim_algo='cosine'):
         if not weight: weight = { 'lyrics': 0.3, 'artist': 0.4, 'genre': 0.4, 'title': 0.5 }
 
         query_vector_lyrics = make_vector(get_tf_idf(get_tf_for_query(self.tf, query), self.idf))
@@ -49,10 +49,10 @@ class VSM:
         query_vector_genre  = make_vector(get_tf_idf(get_tf_for_query(self.tfidf_genre, query), self.idf_genre))
         query_vector_title  = make_vector(get_tf_idf(get_tf_for_query(self.tfidf_title, query), self.idf_title))
 
-        scores_lyrics = { doc_id : cosine(query_vector_lyrics, self.doc_tf_idf_vectors[doc_id])        for doc_id in tqdm(self.get_doc_ids()) }
-        scores_artist = { doc_id : cosine(query_vector_artist, self.doc_tf_idf_vectors_artist[doc_id]) for doc_id in tqdm(self.get_doc_ids()) }
-        scores_genre  = { doc_id : cosine(query_vector_genre , self.doc_tf_idf_vectors_genre[doc_id])  for doc_id in tqdm(self.get_doc_ids()) }
-        scores_title  = { doc_id : cosine(query_vector_title , self.doc_tf_idf_vectors_title[doc_id])  for doc_id in tqdm(self.get_doc_ids()) }
+        scores_lyrics = { doc_id : sim(query_vector_lyrics, self.doc_tf_idf_vectors[doc_id], sim_algo)        for doc_id in tqdm(self.get_doc_ids()) }
+        scores_artist = { doc_id : sim(query_vector_artist, self.doc_tf_idf_vectors_artist[doc_id], sim_algo) for doc_id in tqdm(self.get_doc_ids()) }
+        scores_genre  = { doc_id : sim(query_vector_genre , self.doc_tf_idf_vectors_genre[doc_id], sim_algo)  for doc_id in tqdm(self.get_doc_ids()) }
+        scores_title  = { doc_id : sim(query_vector_title , self.doc_tf_idf_vectors_title[doc_id], sim_algo)  for doc_id in tqdm(self.get_doc_ids()) }
 
         scores = { doc_id: weight['lyrics'] * scores_lyrics[doc_id] + weight['artist'] * scores_artist[doc_id] + weight['genre'] * scores_genre[doc_id] + weight['title'] * scores_title[doc_id] for doc_id in tqdm(self.get_doc_ids()) }
         return sorted_by_value(scores), { 'lyrics': scores_lyrics, 'artist': scores_artist, 'genre': scores_genre, 'title': scores_title }
--- ir/similarities.py
+++ ir/similarities.py
@@ -1,6 +1,28 @@
 import numpy as np
 import math
 
+def dot(v1, v2):
+	score = np.dot(v1, v2)
+	return score if not math.isnan(score) else 0.0
+
 def cosine(v1, v2):
     score = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
     return score if not math.isnan(score) else 0.0
+
+def jackard(v1, v2):
+	score = np.dot(v1, v2) / (np.linalg.norm(v1)**2 + np.linalg.norm(v2)**2 - np.dot(v1, v2))
+	return score if not math.isnan(score) else 0.0
+
+def dice(v1, v2):
+	score = 2 * np.dot(v1, v2) / (np.linalg.norm(v1)**2 * np.linalg.norm(v2)**2)
+	return score if not math.isnan(score) else 0.0
+
+def sim(v1, v2, sim='cosine'):
+	if (sim == 'cosine'):
+		return cosine(v1, v2)
+	elif (sim == 'dice'):
+		return dice(v1, v2)
+	elif (sim == 'dot'):
+		return dot(v1, v2)
+	elif (sim == 'jackard'):
+		return jackard(v1, v2)
